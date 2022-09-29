22a365346d8b3159dda6d622085c0fa1970783f8
Anusha <nssranusha@yahoo.co.in>
2022-09-29 22:52:42 +0530

    Commit: mapping for results

--- models/interview.js
+++ models/interview.js
@@ -8,7 +8,27 @@ const interviewSchema = new mongoose.Schema({
     details: {
         type: String,
         required: true
-    }
+    },
+    
+    //list of array  to get the student data , mapping to set the status of the interview attended
+    studentsList: [
+        {
+            student: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Student'
+            },
+            held_on_date: {
+                type: Date,
+                required: true
+            },
+            results: {
+                type: String,
+                required: true,
+                enum: [PASS, FAIL, ON_HOLD, DIDNT_ATTEMPT]
+            }
+        },
+
+    ],
 
 }, {
     timestamps: true
